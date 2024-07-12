a9886eb4477d532653f349cff3dc3e321b702f35
Keter <ktitus@kabarak.ac.ke>
2024-07-12 21:31:30 +0300

    middleware update

--- routes/users.js
+++ routes/users.js
@@ -29,14 +29,36 @@ router.post('/login', async (req, res) => {
 });
 
 // Middleware to verify token
-const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) return res.sendStatus(403);
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
+// const authenticateToken = (req, res, next) => {
+//     const token = req.headers['authorization'];
+//     if (!token) return res.sendStatus(403);
+//     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//         if (err) return res.sendStatus(403);
+//         req.user = user;
+//         next();
+//     });
+// };
+
+const authenticateToken = async (req, res, next) => {
+    const authHeader = req.header('Authorization');
+    console.log(`Token:${authHeader}`);
+    //   if (!authHeader) return res.status(401).json({ status: 'failure', message: 'Access Denied: No token provided' });
+
+    const token = authHeader.replace('Bearer ', '');
+    //   if (!token) return res.status(401).json({ status: 'failure', message: 'Access Denied: Token missing' });
+    console.log(`Token:${token}`);
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(verified.userId);
+        console.log(`verifiec.id:${verified.userId}`);
+        console.log(`id:${req.user}`);
+        if (!req.user) {
+            return res.status(401).json({ status: 'failure', message: 'Access Denied: User not found' });
+        }
         next();
-    });
+    } catch (err) {
+        res.status(400).json({ status: 'failure', message: 'Invalid Token' });
+    }
 };
 
 module.exports = { router, authenticateToken };
